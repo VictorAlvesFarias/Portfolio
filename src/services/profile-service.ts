d9056791e62c9c3037107ebc29775534b3ba51d4
Victor Alves Farias <victoralvesfarias2004@gmail.com>
2024-12-02 10:35:16 -0300

    Update get assets

--- src/services/profile-service.ts
+++ src/services/profile-service.ts
@@ -1,33 +1,20 @@
 import { RepositorieEntity } from "@/interfaces/entities/repository-entity"
 
 export async function GetProfileDatas() {
-    const data:any =  await fetch("https://raw.githubusercontent.com/VictorAlvesFarias/Portfolio/database/packagePreview.json",{ cache: 'no-store' }) 
-    
-    const repos:RepositorieEntity = await data.json()
+  const data: any = await fetch("https://raw.githubusercontent.com/VictorAlvesFarias/Portfolio/database/packagePreview.json", { cache: 'no-store' })
 
-    const technologies = repos.technologies.map(item=>{
-      return {
-          name:item.name,
-          src:`https://raw.githubusercontent.com/VictorAlvesFarias/Portfolio/database/projects/${item.name.split(" ").join("-").toLocaleLowerCase()}.svg`
-      }
-    })
+  const repos: RepositorieEntity = await data.json()
 
-    const projects = repos.projects.map(item=>{
-      return {
-        ...item,
-        icon: `https://raw.githubusercontent.com/VictorAlvesFarias/Portfolio/database/views/${item.name.split(" ").join("-").toLocaleLowerCase()}/icon-view.png`,
-        images:[
-          {src:`https://raw.githubusercontent.com/VictorAlvesFarias/Portfolio/database/views/${item.name.split(" ").join("-").toLocaleLowerCase()}/view-1.png`},
-          {src:`https://raw.githubusercontent.com/VictorAlvesFarias/Portfolio/database/views/${item.name.split(" ").join("-").toLocaleLowerCase()}/view-2.png`},
-          {src:`https://raw.githubusercontent.com/VictorAlvesFarias/Portfolio/database/views/${item.name.split(" ").join("-").toLocaleLowerCase()}/view-3.png`},
-        ]
-      }
-    })
-
-    const response = {
-      projects,
-      technologies
+  const technologies = repos.technologies.map(item => {
+    return {
+      name: item.name,
+      src: `https://raw.githubusercontent.com/VictorAlvesFarias/Portfolio/database/projects/${item.name.split(" ").join("-").toLocaleLowerCase()}.${item.name.split('.')[item.name.split('.').length - 1]}`
     }
+  })
 
-    return  response 
+  const response = {
+    technologies
   }
+
+  return response
+}
