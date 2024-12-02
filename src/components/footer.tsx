7e6bb199d0231c5bf323af7b7417903275d37e85
Victor Alves Farias <victoralvesfarias2004@gmail.com>
2024-12-02 14:24:47 -0300

    Fix styles

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -11,9 +11,9 @@ import {
 
 function Footer() {
     return (
-        <footer className="border-t-2 border-zinc-300 dark:border-zinc-800">
-            <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8 flex flex-col gap-8">
-                <h2 className="w-full sm:min-w-min flex justify-start sm:justify-center text-lg lg:text-2xl ">
+        <footer className="items-center w-full justify-center flex flex-col my-20 border-t-2 border-zinc-300 dark:border-zinc-800">
+            <div className="max-w-128 w-11/12 lg:px-0 px-5 flex flex-col gap-8 ">
+                <h2 className="w-full sm:min-w-min flex justify-start sm:justify-center text-lg lg:text-2xl mt-8">
                     <div className=' font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-rose-400 to-violet-600 ml-2 '>THANK YOU FOR COMING HERE</div>
                 </h2>
                 <hr className="border-zinc-300 dark:border-zinc-800" />
--- src/components/tecnologie-card.tsx
+++ src/components/tecnologie-card.tsx
@@ -3,10 +3,12 @@ import Image from 'next/image'
 
 function TecnologieCard({ data }: any) {
   return (
-    <div className="group/tc group-hover:first-line:bg-zinc-100 shadow-sm bg-zinc-100 dark:bg-zinc-800  overflow-hidden  flex-col relative flex-1 h-48 flex items-center transition-all justify-center ">
-      <div className='flex flex-col items-center gap-3'>
-        <Image alt='' width={60} height={60} src={data.src}></Image>
-        <div className='font-semibold md:text-1xl'>
+    <div className="group/tc group-hover:first-line:bg-zinc-100 w-full shadow-sm bg-zinc-100 dark:bg-zinc-800  overflow-hidden  flex-col relative flex-1 h-48 flex items-center transition-all justify-center ">
+      <div className='flex flex-col  gap-3 w-full'>
+        <div className='w-full flex items-center justify-center'>
+          <Image alt='' width={60} height={60} src={data.src}></Image>
+        </div>
+        <div className='font-semibold md:text-1xl text-center overflow-hidden text-ellipsis px-6 whitespace-nowrap'>
           {data.name}
         </div>
       </div>
