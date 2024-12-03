4fc13d83a3404e39294f93c2de4e095b45c22743
Victor Alves Farias <victoralvesfarias2004@gmail.com>
2024-12-03 13:32:36 -0300

    Navbar fix

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -13,11 +13,8 @@ import { Languages } from '../../i18n.config';
 import Locale from './locale';
 
 function Navbar() {
-
   const [navStyle, setNavStyle] = useState(false);
-
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
-
   const [mobileMenuLanguage, setMobileMenuLanguage] = useState(false)
 
   useEffect(() => {
@@ -45,19 +42,19 @@ function Navbar() {
       <header className={`fixed w-screen transition flex top-0 left-0 justify-center items-center z-30 ${navStyle ? ' bg-zinc-50 shadow-lg  dark:bg-zinc-900 ' : ' '}`}>
         <Container>
           <nav className="flex items-center font-semibold justify-between lg:justify-center h-20 gap-x-12 ">
-            <div onClick={() => setMobileMenuOpen(false)} className="hidden lg:block">
-              <Anch href="/#about-me" className={`text-md`}>{texts.aboutMe}</Anch>
+            <Anch href="/#about-me" className="hidden lg:block">
+              <span className={`text-md`}>{texts.aboutMe}</span>
               <Gradientline />
-            </div>
-            <div onClick={() => setMobileMenuOpen(false)} className="hidden lg:block">
-              <Anch href="/#technologies" className={`text-md`}>{texts.technologies}</Anch>
+            </Anch>
+            <Anch href="/#technologies" className="hidden lg:block">
+              <span className={`text-md`}>{texts.technologies}</span>
               <Gradientline />
-            </div>
-            <div onClick={() => setMobileMenuOpen(false)} className="hidden lg:block">
-              <Anch href="/#links" className={`text-md`}>{texts.links}</Anch>
+            </Anch>
+            <Anch href="/#links" className="hidden lg:block">
+              <span className={`text-md`}>{texts.links}</span>
               <Gradientline />
-            </div>
-            <DropdownMenu.Root >
+            </Anch>
+            <DropdownMenu.Root>
               <DropdownMenu.Trigger asChild>
                 <div className="hidden lg:block">
                   <button className={`flex justify-center items-center text-md`} aria-label="Customise options">
@@ -107,30 +104,30 @@ function Navbar() {
               <div className="mt-6 flow-root">
                 <div className="-my-6 divide-y divide-gray-500/10">
                   <div className="flex flex-col gap-6">
-                    <div onClick={() => setMobileMenuOpen(false)} className="block ">
-                      <Anch href="/#about-me" className={`text-md`}>{texts.aboutMe}</Anch>
+                    <Anch href="/#about-me" onClick={() => setMobileMenuOpen(false)} className="block">
+                      <span className={`text-md`}>{texts.aboutMe}</span>
                       <Gradientline />
-                    </div>
-                    <div onClick={() => setMobileMenuOpen(false)} className="block">
-                      <Anch href="/#technologies" className={`text-md`}>{texts.technologies}</Anch>
+                    </Anch>
+                    <Anch href="/#technologies" onClick={() => setMobileMenuOpen(false)} className="block">
+                      <span className={`text-md`}>{texts.technologies}</span>
                       <Gradientline />
-                    </div>
-                    <div onClick={() => setMobileMenuOpen(false)} className="block">
-                      <Anch href="/#links" className={`text-md`}>{texts.links}</Anch>
+                    </Anch>
+                    <Anch href="/#links" onClick={() => setMobileMenuOpen(false)} className="block">
+                      <span className={`text-md`}>{texts.links}</span>
                       <Gradientline />
-                    </div>
-                    <div onClick={() => setMobileMenuLanguage(!mobileMenuLanguage)} >
+                    </Anch>
+                    <div onClick={() => setMobileMenuLanguage(!mobileMenuLanguage)}>
                       <span className={`pl-3 text-md cursor-pointer`}>{texts.language} </span>
                       <Gradientline />
-                      <div className={`pl-10  flex flex-col gap-6 transition-all overflow-hidden ${mobileMenuLanguage ? "h-32" : "h-0"}`}>
-                        <div onClick={() => setMobileMenuOpen(false)} className="block mt-6">
-                          <Locale href="pt-br" className={`pl-3 text-md`}>Português</Locale>
+                      <div className={`pl-10 flex flex-col gap-6 transition-all overflow-hidden ${mobileMenuLanguage ? "h-32" : "h-0"}`}>
+                        <Anch href="pt-br" onClick={() => setMobileMenuOpen(false)} className="block mt-6">
+                          <span className={`pl-3 text-md`}>Português</span>
                           <Gradientline />
-                        </div>
-                        <div onClick={() => setMobileMenuOpen(false)} className="block">
-                          <Locale href="en-us" className={`pl-3 text-md`}>English</Locale>
+                        </Anch>
+                        <Anch href="en-us" onClick={() => setMobileMenuOpen(false)} className="block">
+                          <span className={`pl-3 text-md`}>English</span>
                           <Gradientline />
-                        </div>
+                        </Anch>
                       </div>
                     </div>
                   </div>
